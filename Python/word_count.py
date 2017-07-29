d761635129ede9344d0e3e1f6e11b568e168e7ea
juwitaw <sakura181094@yahoo.co.uk>
2017-07-30 00:24:02 +0700

    add python word count with rdd

--- Python/word_count.py
+++ Python/word_count.py
@@ -4,10 +4,20 @@ spark = SparkSession.builder \
 	.master("local[*]") \
 	.appName("word_count") \
 	.getOrCreate()
-sc - spark.sparkContext
+sc = spark.sparkContext
 
-# text_file = sc.textFile("hdfs://...")
-# counts = text_file.flatMap(lambda line: line.split(" ")) \
-#              .map(lambda word: (word, 1)) \
-#              .reduceByKey(lambda a, b: a + b)
-# counts.saveAsTextFile("hdfs://...")
+#rdd-based
+text_rdd = sc.parallelize(["Apache Spark is a fast and general-purpose cluster computing system", 
+"It provides high-level APIs in Java, Scala, Python and R, and an optimized engine that \
+supports general execution graphs", "It also supports a rich set of higher-level tools including \
+Spark SQL for SQL and structured data processing, MLlib for machine learning, GraphX for graph \
+processing, and Spark Streaming"])
+counts = text_rdd.flatMap(lambda line: line.split(" ")) \
+             .map(lambda word: (word, 1)) \
+             .reduceByKey(lambda a, b: a + b)
+result = counts.collect()
+print(result)
+
+#dataframe-based
+text_df = text_rdd.toDF()
+text_df.show()
